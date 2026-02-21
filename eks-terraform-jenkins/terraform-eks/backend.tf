fdde7a1797a01896af3fc86ce62ba72c7eaec4d9
Ranjeeth Reddy S <109015686+ranjireddy041@users.noreply.github.com>
2026-02-21 14:56:53 +0530

    Update backend.tf

--- eks-terraform-jenkins/terraform-eks/backend.tf
+++ eks-terraform-jenkins/terraform-eks/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "ranjith-devops-terraform-bucket-001-us" # Replace with your actual S3 bucket name
     key    = "todo-app/terraform.tfstate"
-    region = "ua-east-1"
+    region = "us-east-1"
   }
 }
